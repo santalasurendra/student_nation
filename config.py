a50aca57b6295eee2ab0f598b8a9217c03cdc20a
SANTALA SURENDRA <surendrasaantala@gmail.com>
2026-03-15 08:08:45 +0530

    Update database URI to use student_nation.db

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'santhiram_engineering_college_srec_secret_key'
     BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'database', 'database.db')
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'student_nation.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     
     # Upload folders
